1220ed37cd12246a4af87b957ada53e8c7bc6d3b
Artsem Varyvoda <artemvarivoda@gmail.com>
2019-10-28 18:15:42 +0300

    Upload tasks for 4th Module

---
+++ Module4/Module4_1/Module3_1/Module3_1/Program.cs
@@ -0,0 +1,104 @@
+﻿/*Array is hardcoded.
+ 1. A ArrayMax method
+ 1. Б ArrayMax method
+ 1. B ArraySum method
+ 1. Г ArrayMaxMinDifference method
+ 1. Д ArrayOddsAndOdd method*/
+
+using System;
+
+
+namespace Module3_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] balance = new int[] { 1, -2, 30, 999, -21, 2300, 84, 500, 0 };
+            Console.WriteLine($"Max value in the array is:{ArrayMax(balance)}");
+            Console.WriteLine($"Miminum value in the array is: {ArrayMin(balance)}");
+            Console.WriteLine($"Sum of all elements in the array is: {ArraySum(balance)}");
+            Console.WriteLine(ArrayMaxMinDifference(balance));
+            ArrayOddsAndOdd(balance);
+        }
+
+        static int ArrayMax(int[] array)
+        {
+            int maxValue = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+
+                if (array[i] > maxValue)
+                    maxValue = array[i];
+            }
+            return maxValue;
+        }
+
+        static int ArrayMin(int[] array)
+        {
+            int minValue = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+
+                if (array[i] < minValue)
+                    minValue = array[i];
+            }
+            return minValue;
+        }
+
+        static int ArraySum(int[] array)
+        {
+            int sum = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                sum += array[i];                
+            }
+            return sum;
+        }
+
+        static int ArrayMaxMinDifference(int[] array)
+        {
+            int maxValue = array[0];
+            int minValue = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+
+                if (array[i] > maxValue)
+                    maxValue = array[i];
+                if (array[i] < minValue)
+                    minValue = array[i];
+            }
+            return maxValue-minValue;
+        }
+
+        static int[] ArrayOddsAndOdd(int[] array)
+        {
+            int maxValue = array[0];
+            int minValue = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+
+                if (array[i] > maxValue)
+                    maxValue = array[i];
+                if (array[i] < minValue)
+                    minValue = array[i];
+            }
+
+            for (int i = 0; i< array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                    array[i] = array[i] + maxValue;
+                else array[i] = array[i] - minValue;
+            }
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            return array;
+        }
+
+    }
+}
+
+
+    
---
+++ Module4/Module4_4/Module3_4/Program.cs
@@ -0,0 +1,66 @@
+﻿/* 
+4 А. IncrementalBy10 method method
+4 Б. CircleCircumferenceArea method
+4 В. ArrayMaxMinSum method
+It was interesting task, learn a lot about tuples :)
+*/
+using System;
+
+namespace Module3_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            int firstValue = 10;
+            int secondValue = 15;
+            int thirdValue = 23;
+            Console.WriteLine($"For now values is: {firstValue}, {secondValue}, {thirdValue}");
+            var tuple = IncrementalBy10(firstValue, secondValue, thirdValue);
+            Console.WriteLine($"If we use method IncrementalBy10, tuple elements will be {tuple.Item1}, {tuple.Item2}, {tuple.Item3}\n");
+            double radius = 10;
+            var (circumference, area) = CircleCircumferenceArea(radius);
+            Console.WriteLine($"For the {radius} radius of the Circle it's Length will be {circumference}, and Area: {area}\n");
+            int[] array = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("Elements of the array are: ");
+            foreach (var arrayElement in array)
+            {
+                Console.Write(arrayElement + " ");
+            }
+            var (maxValue, minValue, sumOfElements) = ArrayMaxMinSum(array);
+            Console.WriteLine($"\nMaximum element in the array: {maxValue}, mininum element {minValue}, sum of all elements: {sumOfElements}");
+        }
+
+        public static (int, int, int) IncrementalBy10(int value1, int value2, int value3)
+        {
+            var result = (value1 + 10, value2 + 10, value3 + 10);
+            return result;
+        }
+
+        public static (double circumference, double area) CircleCircumferenceArea(double radius)
+        {
+            double circumference = 2 * Math.PI * radius;
+            double area = Math.PI * Math.Pow(radius, 2);
+            var result = (circumference, area);
+            return result;
+        }
+
+        static (int maxValue, int minValue, int sumOfElements) ArrayMaxMinSum(int[] array)
+        {
+            int maxValue = array[0];
+            int minValue = array[0];
+            int sumOfElements = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (minValue > array[i])
+                    minValue = array[i];
+                else if (maxValue < array[i])
+                    maxValue = array[i];
+                sumOfElements += array[i];
+            }
+
+            var result = (maxValue, minValue, sumOfElements);
+            return result;
+        }
+    }
+}
